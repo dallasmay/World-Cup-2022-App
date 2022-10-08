ae2a5d319334ab164eb3627be94983fc64217f2e
dallasmay <dmmay9@gmail.com>
2022-10-08 16:43:47 -0600

    added firebase create account functionality to signupform

--- src/components/authentication/SignupForm/SignupForm.js
+++ src/components/authentication/SignupForm/SignupForm.js
@@ -1,19 +1,60 @@
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 import styles from "./SignupForm.module.css";
 
 const SignupForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const auth = getAuth();
+
+  const signupSubmitHandler = (evt) => {
+    evt.preventDefault();
+    if (password === confirmPassword && email.includes("@")) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log(userCredential);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Passwords need to match")
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={signupSubmitHandler}>
         <label htmlFor="name-signup">Name:</label>
-        <input type="text" id="name-signup" />
+        <input
+          type="text"
+          id="name-signup"
+          onChange={(evt) => setName(evt.target.value)}
+        />
         <label htmlFor="email-signup">Email:</label>
-        <input type="email" id="email-signup" />
+        <input
+          type="email"
+          id="email-signup"
+          onChange={(evt) => setEmail(evt.target.value)}
+        />
         <label htmlFor="password-signup">Password:</label>
-        <input type="password" id="password-signup" />
-        <button>Register</button>
+        <input
+          type="password"
+          id="password-signup"
+          onChange={(evt) => setPassword(evt.target.value)}
+        />
+        <label htmlFor="password-confirm-signup">Confirm Password:</label>
+        <input
+          type="password"
+          id="password-confirm-signup"
+          onChange={(evt) => setConfirmPassword(evt.target.value)}
+        />
+        <button type="submit">Register</button>
       </form>
     </>
   );
-}
+};
 
 export default SignupForm;
