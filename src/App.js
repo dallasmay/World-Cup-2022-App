8aa1216bec753dfc30e5c6f608276f4a336b1936
dallasmay <dmmay9@gmail.com>
2022-10-08 13:55:31 -0600

    revoked firebase credentials previously committed and added new ones to .env file

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJtgCjKUxJq8jdBvU1zIloSq1rXwiTmzA",
-  authDomain: "world-cup-5730e.firebaseapp.com",
-  projectId: "world-cup-5730e",
-  storageBucket: "world-cup-5730e.appspot.com",
-  messagingSenderId: "504024501178",
-  appId: "1:504024501178:web:0e364c1305c96e11116b83",
-  measurementId: "G-CF9TD0RWV2"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
 // Initialize Firebase
