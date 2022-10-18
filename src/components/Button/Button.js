b833d7f15334f2b98dae94653df5374cb4619d9b
dallasmay <dmmay9@gmail.com>
2022-10-18 17:34:56 -0600

    created reusable button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import styles from "./Button.module.css";
+
+const Button = (props) => {
+  return <button type={`${props.type}`} className={styles.btn}>{props.children}</button>
+}
+
+export default Button;
