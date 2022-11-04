c6344b0f4113c5f48e65b4620bec38a2b38e8438
dallasmay <dmmay9@gmail.com>
2022-11-04 11:22:23 -0600

    drag and drop functionality working correctly

--- src/components/groupSelection/CountryCard/CountryCard.js
+++ src/components/groupSelection/CountryCard/CountryCard.js
@@ -1,8 +1,25 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 import styles from "./CountryCard.module.css";
 
-const CountryCard = ({ name }) => {
+const CountryCard = ({ id, name }) => {
+  const { setNodeRef, attributes, listeners, transition, transform } =
+    useSortable({ id: id });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
-    <div className={styles["country-card"]}>
+    <div
+      className={styles["country-card"]}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+    >
       <h1>CountryCard: {name}</h1>
     </div>
   );
--- src/components/groupSelection/GroupSortingContainer/GroupSortingContainer.js
+++ src/components/groupSelection/GroupSortingContainer/GroupSortingContainer.js
@@ -1,14 +1,73 @@
+import { useState } from "react";
+
+import {
+  DndContext,
+  closestCenter,
+  useSensor,
+  useSensors,
+  PointerSensor,
+  TouchSensor,
+  KeyboardSensor,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { restrictToVerticalAxis, restrictToWindowEdges } from "@dnd-kit/modifiers";
+
 import CountryCard from "../CountryCard/CountryCard";
 
 import styles from "./GroupSortingContainer.module.css";
 
 const GroupSortingContainer = () => {
+  const [countriesArr, setCountriesArr] = useState([
+    { id: "1", name: "USA" },
+    { id: "2", name: "Wales" },
+    { id: "3", name: "England" },
+    { id: "4", name: "Iran" },
+  ]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragEnd = ({ active, over }) => {
+    if (active.id !== over.id) {
+      setCountriesArr((prevState) => {
+        const oldIndex = prevState.findIndex(
+          (country) => country.id === active.id
+        );
+        const newIndex = prevState.findIndex(
+          (country) => country.id === over.id
+        );
+        return arrayMove(prevState, oldIndex, newIndex); 
+      })
+    }
+  };
+
   return (
     <div className={styles["group-sorting-container"]}>
-      <CountryCard name="USA" />
-      <CountryCard name="Wales" />
-      <CountryCard name="England" />
-      <CountryCard name="Iran" />
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+        modifiers={[restrictToVerticalAxis, restrictToWindowEdges]}
+      >
+        <SortableContext
+          items={countriesArr.map((country) => country.id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {countriesArr.map((country) => {
+            return <CountryCard {...country} key={country.id} />;
+          })}
+        </SortableContext>
+      </DndContext>
     </div>
   );
 };
