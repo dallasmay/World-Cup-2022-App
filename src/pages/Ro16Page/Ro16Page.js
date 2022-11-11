9ec0267c319def3e6d7591dda0b1b1e6822fd86c
dallasmay <dmmay9@gmail.com>
2022-11-11 16:03:34 -0700

    added minor styling changes

--- src/pages/Ro16Page/Ro16Page.js
+++ src/pages/Ro16Page/Ro16Page.js
@@ -1,15 +1,23 @@
+import { useSelector } from "react-redux";
+
 import BackToProfile from "../../components/BackToProfile/BackToProfile";
 import BracketNav from "../../components/BracketNav/BracketNav";
+import HeadToHeadCard from "../../components/HeadToHeadCard/HeadToHeadCard";
 
 import styles from "./Ro16Page.module.css";
 
 const Ro16Page = () => {
+  const ro16Arr = useSelector((state) => state.bracket.ro16Arr);
+
   return (
     <>
       <BackToProfile />
       <BracketNav />
       <div className={styles["content-container"]}>
         <h1 className={styles.heading1}>Round of 16</h1>
+        {ro16Arr.map((match) => {
+          return <HeadToHeadCard game={match[2]} countries={[match[0], match[1]]}/>; 
+        })}
       </div>
     </>
   );
