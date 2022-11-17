32996245beacebadafa89c9e447cfb4e21ee899c
dallasmay <dmmay9@gmail.com>
2022-11-16 20:58:36 -0700

    added color change to your team on leaderboard, updated some meta data

--- src/pages/LeaderboardPage/LeaderboardPage.js
+++ src/pages/LeaderboardPage/LeaderboardPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import axios from "axios";
 
 import BackToProfile from "../../components/BackToProfile/BackToProfile";
@@ -10,14 +11,12 @@ const URL = process.env.REACT_APP_SERVER_URL;
 const LeaderboardPage = () => {
   const [leaderboardArr, setLeaderboardArr] = useState([]);
 
-  const name =
-    "Diamond Dyl's Magnificent Party Pals and the basdjfslkjdfklsdflsdflsdklfjsdklf fasdjf asldkfjas;lfa sdlkfjalsk;d asdklfjasdlk;fj";
-
+  const teamName = useSelector((state) => state.auth.teamName);
+  
   useEffect(() => {
     axios
       .get(`${URL}/leaderboard`)
       .then((res) => {
-        console.log(res)
         setLeaderboardArr(res.data);
       })
       .catch((err) => alert(err));
@@ -33,16 +32,15 @@ const LeaderboardPage = () => {
             return (
               <div className={styles["team-container"]}>
                 <p className={styles.rank}>{index + 1}</p>
-                <p className={styles["team-name"]}></p>
                 <div>
-                  <p className={styles["team-name"]}>
+                  <p className={styles["team-name"]} style={ele.team_name === teamName ? {color: "var(--green)"} : {}}>
                     {ele.team_name > 20
                       ? ele.team_name.slice(0, 19) + "..."
                       : ele.team_name}
                   </p>
                   <p className={styles.name}>{ele.name}</p>
                 </div>
-                <p className={styles.points}>{ele.score}</p>
+                <p className={styles.points} style={ele.team_name === teamName ? {color: "var(--green)"} : {}}>{ele.score}</p>
               </div>
             );
           })}
